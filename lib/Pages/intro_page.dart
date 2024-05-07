43fd167932f032efb5ebe41d6b80e84e71b79835
Dinath01 <dinithnethmina101@gmail.com>
2024-05-07 13:07:14 +0530

    Splash Intro Screen Implemented

---
+++ lib/Pages/intro_page.dart
@@ -0,0 +1,40 @@
+import 'package:beskarapp/Pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => HomePage()),
+            );
+          });
+        }
+
+        return Scaffold(
+          backgroundColor: Colors.white,
+          body: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/splashbg.jpg'),
+                  fit: BoxFit
+                      .cover, 
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
